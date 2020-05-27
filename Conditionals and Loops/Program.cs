351b5913e057ad222678f9438c47f7ed700d7074
Pettamah <petrichtomi@gmai.com>
2020-05-27 11:55:53 +0200

    05.27. 12

---
+++ Conditionals and Loops/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conditionals_and_Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ez az utolsó próba");
+        }
+    }
+}

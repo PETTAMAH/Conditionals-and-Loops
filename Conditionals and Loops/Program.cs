98066a02488aed6950d14684d4b651c1f59550bf
petrichtamas <petrichtomi@]mail.com>
2020-05-28 13:27:12 +0200

    05.28. 14

--- Conditionals and Loops/Program.cs
+++ Conditionals and Loops/Program.cs
@@ -139,6 +139,40 @@ namespace Conditionals_and_Loops
             }
             */
 
+            //switch 1.\\
+            /*
+            int num = 2;
+            switch (num)
+            {
+                case 1:
+                    Console.WriteLine("one");
+                    break;
+                case 4:
+                    Console.WriteLine("two");
+                    break;
+                case 3:
+                    Console.WriteLine("three");
+                    break;
+            }
+            */
+
+            //switch 2.\\
+            /*
+            int x = 33;
+            switch (x)
+            {
+                case 8:
+                    Console.WriteLine("Eight");
+                    break;
+                case 18:
+                    Console.WriteLine("Eighteen");
+                    break;
+                case 33:
+                    Console.WriteLine("Thirdty three");
+                    break;
+
+            }
+            */
 
             Console.ReadKey();
         }
